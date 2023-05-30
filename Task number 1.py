7cf198e0ce819b476fd41c4b3c445eb2d64840e8
AhmedHanafy69 <132225353+AhmedHanafy69@users.noreply.github.com>
2023-05-30 18:32:09 +0300

    Update Task number 1.py

--- Task number 1.py
+++ Task number 1.py
@@ -1,7 +1,13 @@
-M_earth = 6.0 * (10**24)
-M_moon = 7.34 * (10**22)
-r = 3.84 * (10**8)
-G = 6.673 * (10**-11)
+M_earth= float(input("Enter M_earth : "))
+M_moon= float(input("Enter M_moon : "))
+r = float(input("Enter r : "))
+G = float(input("Enter G : "))
+
+
+M_earth = M_earth * (10**24)
+M_moon = M_moon * (10**22)
+r = r * (10**8)
+G = G * (10**-11)
 
 fg = (G * M_earth * M_moon) / (r**2)
 print(f"The gravitational force is: {fg} N")
